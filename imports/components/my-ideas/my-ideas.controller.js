13a46419960b105f316b5133a8c1aca6126272b4
Petar Slovic <petar.slovic@gmail.com>
2016-05-01 23:56:36 +0200

    create module

---
+++ imports/components/my-ideas/my-ideas.controller.js
@@ -0,0 +1,27 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import { Ideas } from '../../api/ideas';
+import { Categories } from '../../api/categories';
+
+export class MyIdeasController {
+  constructor($scope, $reactive, $stateParams) {
+    'ngInject';
+
+    this.category = $stateParams.category;
+
+    $reactive(this).attach($scope);
+
+    this.subscribe('ideas', angular.noop, { category: this.category });
+
+    this.helpers({
+      ideas() {
+        return Ideas.find({});
+      },
+      categoryBySlug() {
+        return Categories.findOne({ slug: this.category })
+      }
+    });
+
+  }
+
+}
---
+++ imports/components/my-ideas/my-ideas.js
@@ -0,0 +1,28 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import { MyIdeasController } from './my-ideas.controller';
+ 
+import './my-ideas.html';
+ 
+const name = 'my-ideas';
+
+export default angular.module(name, [
+  angularMeteor
+]).component(_.camelCase(name), {
+  templateUrl: `imports/components/${name}/${name}.html`,
+  controllerAs: _.camelCase(name),
+  controller: MyIdeasController
+}).config(config);
+ 
+function config($stateProvider) {
+  'ngInject';
+  $stateProvider
+    .state('app.my-ideas', {
+      url: '/ideas/my/:category',
+      template: '<my-ideas></my-ideas>',
+      params: {
+        category: {value: null, squash: true}
+      }
+    });
+};
